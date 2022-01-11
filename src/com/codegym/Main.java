5d402661358a9702d7691f7f6783dd5fb17f9c3b
thangnguyen2841990 <96616281+thangnguyen2841990@users.noreply.github.com>
2022-01-11 11:58:43 +0700

    done

---
+++ src/com/codegym/Main.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // Test class Point2D
+        Point2D point2d = new Point2D();
+        System.out.println(point2d);
+        point2d = new Point2D(5, 7);
+        System.out.println(point2d);
+
+
+        //Test class Point3D
+        Point3D point3d = new Point3D();
+        System.out.println(point3d);
+        point3d = new Point3D(1, 2, 3);
+        System.out.println(point3d);
+    }
+}
+
---
+++ src/com/codegym/Point3D.java
@@ -0,0 +1,42 @@
+package com.codegym;
+
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D() {
+    }
+
+    public Point3D(float z) {
+        this.z = z;
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+    public void setXYZ(float x, float y, float z){
+        super.setXY(x,y);
+         this.z = z;
+    }
+    public float[] getXYZ(){
+        float[]arr = new float[3];
+        arr[0] = super.getX();
+        arr[1] = super.getY();
+        arr[2] = this.z;
+        return arr;
+
+    }
+
+    @Override
+    public String toString() {
+       return "x =" + super.getX() +", y= "+super.getY()+", z= "+this.z;
+    }
+}
